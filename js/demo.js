8fe2a00eaec49aefced1ed8ba6b94d38ce5d8733
OF1224 <xierongqin@qianmi.com>
2017-05-16 14:54:53 +0800

    Mobile UI 第一次提交

---
+++ js/demo.js
@@ -0,0 +1,35 @@
+
+//$(window).scroll(function(){
+
+        $(".side-menu li").click(function(){
+                $(this).addClass("active");
+                if($(this).has("ul")){
+                        $(this).children("ul").show("slow");
+                        $(".side-menu li ul li").click(function(){
+                                $(this).addClass("active").siblings().removeClass("active");
+                        })
+                }
+                $(this).siblings().removeClass("active");
+                $(this).siblings().children("ul").hide("slow");
+        })
+
+        var scrollTop = $(window).scrollTop();
+
+        if(scrollTop > 102 ){
+                $(".side-menu").css("position","fixed");
+        }
+        else{
+                $(".side-menu").css("position","relative");
+        }
+        if(scrollTop>400){
+                $(".scroll-top-btn").show();
+                $(".scroll-top-btn").click(function(e){
+                        e.preventDefault();
+                        $("html,body").animate({scrollTop:0},800)
+                })
+        }
+        else{
+                $(".scroll-top-btn").hide();
+        }
+
+//})
